7adbeea9b62772a41ace01b981a2059c05851658
Carl Omondi <carlkomondi@gmail.com>
2022-06-18 14:03:58 -0500

    created media player

--- src/typescript/views/app/app.tsx
+++ src/typescript/views/app/app.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { AlbumPage } from "../album/AlbumPage";
 import { ArtistPage } from "../artist/ArtistPage";
 import { HomePage } from "../home/home";
+import { MediaPlayer } from "../media/mediaPlayer";
 import { NavBar } from "../nav/navbar";
 import { SearchPage } from "../search/SearchPage";
 
@@ -30,10 +31,12 @@ class App extends React.Component
 
             <Routes>
                 <Route exact index element={ <HomePage /> } />
-                <Route path="/album/:id" element={ <AlbumPage /> } />
-                <Route path="/artist/:id" element={ <ArtistPage /> } />
+                <Route path="/album/*" element={ <AlbumPage /> } />
+                <Route path="/artist/*" element={ <ArtistPage /> } />
                 <Route path="/search/*" element={ <SearchPage /> } />
             </Routes>
+
+            <MediaPlayer />
         </>
         )
     }
---
+++ src/typescript/views/media/mediaPlayer.tsx
@@ -0,0 +1,54 @@
+
+
+
+
+
+
+
+
+
+
+import * as React from "react";
+
+
+
+
+interface IMediaPlayerProperties 
+{
+    
+}
+
+function MediaPlayer(props: IMediaPlayerProperties)
+{
+
+
+    return (
+
+        <section id="media">
+        <div className="content">
+            <h1>media player</h1>
+        </div>
+        </section>
+    )
+
+}
+
+export { MediaPlayer }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
